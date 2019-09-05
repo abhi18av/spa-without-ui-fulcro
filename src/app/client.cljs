5193b19212996250862b2729b33df24bd0dad6b1
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-09-05 14:21:32 +0530

    nest components

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -5,10 +5,22 @@
             [com.fulcrologic.fulcro.dom :as dom]
             [com.fulcrologic.fulcro.algorithms.merge :as merge]
             [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defsc Sample [this props]
+;; NOTE:  {:keys [:person/name]} AND  {:person/keys [name]} are equivalent
+
+(defsc Person [this {:keys [:person/id :person/name] :as props}]
+       {}
+       (dom/div "Name: " name))
+
+(def ui-person (comp/factory Person {:keyfn :person/id}))
+
+
+(defsc Sample [this {:keys [sample]}]
   {}
-  (dom/div "Hello, World!"))
+  (dom/div
+    (dom/div "Hello, World!")
+    (dom/div (ui-person sample))))
 
 (defonce APP (app/fulcro-app))
 
@@ -18,8 +30,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (comment
-  (shadow/repl :main)
+(shadow/repl :main)
+
+(::app/state-atom APP)
+(keys APP)
+
+(-> APP
+    (::app/state-atom)
+    deref)
+
+(reset! (::app/state-atom APP) {:a 1})
+
+(app/schedule-render! APP)
+
+(reset! (::app/state-atom APP) {:sample {:person/id 1
+                                         :person/name "Joe"}})
 
 
-
-  )
+)
