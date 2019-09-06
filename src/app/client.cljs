9418e20fe7d7a854168166f08e1793ce782ea4a4
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-09-06 21:20:25 +0530

    reformatted the code via cursive

--- src/app/client.cljs
+++ src/app/client.cljs
@@ -10,16 +10,15 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
 (defsc Car [this {:car/keys [id model] :as props}]
-       {:query         [:car/id :car/model]
-        :initial-state (fn [{:keys [id model]}]
-                           {:car/id    id
-                            :car/model model})
-        :ident         :car/id
-        ;; NOTE optional elements within a component
-        :some-random-data "random data"}
-       (dom/div "Model: " model))
+  {:query            [:car/id :car/model]
+   :initial-state    (fn [{:keys [id model]}]
+                       {:car/id    id
+                        :car/model model})
+   :ident            :car/id
+   ;; NOTE optional elements within a component
+   :some-random-data "random data"}
+  (dom/div "Model: " model))
 
 (def ui-car (comp/factory Car {:keyfn :car/id}))
 
@@ -30,64 +29,62 @@
   #_(rest [env] true)
   (action [{:keys [state]}]
           (swap! state update-in [:person/id id
-                                  :person/age] inc )))
+                                  :person/age] inc)))
 
 ;; NOTE:  {:keys [:person/name]} AND  {:person/keys [name]} are equivalent
 (defsc Person [this #_{:keys [:person/id :person/name :person/age :person/cars] :as props}
                {:person/keys [id name age cars] :as props}]
 
-       {:query         [:person/id :person/name :person/age #_:person/cars
-                        {:person/cars (comp/get-query Car)}]
-        #_:initial-state #_(fn [{:keys [id name]}]
-                           {:person/id   id
-                            :person/name name
-                            :person/age  33
-                            :person/cars [(comp/get-initial-state Car {:id 0 :model "Feet"})
-                                          (comp/get-initial-state Car {:id 1 :model "Wheel"})]})
-        :initial-state {:person/id :param/id
-                        :person/name :param/name
-                        :person/age 33
-                        :person/cars [{:id 0 :model "Feet"}
-                                       {:id 1 :model "Wheel"}]}
-        :ident         :person/id
-        ;; NOTE react lifecycle methods ( should component is provided by default in fulcro3
-        #_:shouldComponentUpdate #_(fn [this props state])
-        :componentDidMount (fn [this]
-                             (let [p (comp/props this)]
-                                  (js/console.log "MOUNTED" p)))
-        :initLocalState (fn [this props]
-                             {:onClick (fn [evt] (js/console.log "Clicked on Name in Person Component"))})
-        }
+  {:query             [:person/id :person/name :person/age #_:person/cars
+                       {:person/cars (comp/get-query Car)}]
+   #_:initial-state #_(fn [{:keys [id name]}]
+                        {:person/id   id
+                         :person/name name
+                         :person/age  33
+                         :person/cars [(comp/get-initial-state Car {:id 0 :model "Feet"})
+                                       (comp/get-initial-state Car {:id 1 :model "Wheel"})]})
+   :initial-state     {:person/id   :param/id
+                       :person/name :param/name
+                       :person/age  33
+                       :person/cars [{:id 0 :model "Feet"}
+                                     {:id 1 :model "Wheel"}]}
+   :ident             :person/id
+   ;; NOTE react lifecycle methods ( should component is provided by default in fulcro3
+   #_:shouldComponentUpdate #_(fn [this props state])
+   :componentDidMount (fn [this]
+                        (let [p (comp/props this)]
+                          (js/console.log "MOUNTED" p)))
+   :initLocalState    (fn [this props]
+                        {:onClick (fn [evt] (js/console.log "Clicked on Name in Person Component"))})}
 
-(let [onClick (comp/get-state this :onClick)]
-       (dom/div :.ui.form                                   ;; {:className "ui form"}
-         (dom/div :.ui.field {:style {:color "red"}}
-                  (dom/label {:onClick onClick} "Name: ")
-                  name)
-         (dom/div :.ui.field
-           (dom/label  "Age: ")
-           age)
-         (dom/button :.ui.positive.basic.button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "make-older")
-         (dom/h3 "Cars: ")
-         (dom/ul (map ui-car cars)))))
+  (let [onClick (comp/get-state this :onClick)]
+    (dom/div :.ui.form                                      ;; {:className "ui form"}
+             (dom/div :.ui.field {:style {:color "red"}}
+                      (dom/label {:onClick onClick} "Name: ")
+                      name)
+             (dom/div :.ui.field
+                      (dom/label "Age: ")
+                      age)
+             (dom/button :.ui.positive.basic.button {:onClick #(comp/transact! this [(make-older {:person/id id})])} "make-older")
+             (dom/h3 "Cars: ")
+             (dom/ul (map ui-car cars)))))
 
 (def ui-person (comp/factory Person {:keyfn :person/id}))
 
 ;;;;;;;;;;;
 
 (defsc Sample [this {:root/keys [person]}]
-       {:query         [{:root/person (comp/get-query Person)}]
-        ;; NOTE alternate notation for expressing initial-state
-        #_:initial-state #_(fn [_] {:root/person (comp/get-initial-state Person {:id 1 :name "Adam"})})
-        :initial-state {:root/person {:id 1 :name "Adam"}}}
-       (dom/div
-         #_(dom/div "Hello, World!")
-         (dom/div (ui-person person))))
+  {:query         [{:root/person (comp/get-query Person)}]
+   ;; NOTE alternate notation for expressing initial-state
+   #_:initial-state #_(fn [_] {:root/person (comp/get-initial-state Person {:id 1 :name "Adam"})})
+   :initial-state {:root/person {:id 1 :name "Adam"}}}
+  #_(dom/div
+      (dom/div (ui-person person))))
 
 (defonce APP (app/fulcro-app))
 
 (defn ^:export init []
-      (app/mount! APP Sample "app"))
+  (app/mount! APP Sample "app"))
 
 
 
@@ -97,13 +94,12 @@
   (shadow/repl :main)
 
   (::app/state-atom APP)
+
   (keys APP)
 
   (-> APP
       (::app/state-atom)
       deref)
-
-  (reset! (::app/state-atom APP) {:a 1})
 
   (app/schedule-render! APP)
 
@@ -111,6 +107,7 @@
                                            :person/name "Joe"
                                            :person/cars [{:car/id    22
                                                           :car/model "Escort"}]}})
+
   (app/current-state APP)
 
   (reset! (::app/state-atom APP) {})
@@ -129,7 +126,7 @@
                                                      :car/model "BMW"}]})
 
   (merge/merge-component! APP Person {:person/id   3
-                                      :person/age 45
+                                      :person/age  45
                                       :person/name "Billy"
                                       :person/cars [{:car/id    24
                                                      :car/model "Ferrari"}]}
@@ -174,15 +171,14 @@
 
   (swap! (::app/state-atom APP) assoc-in [:person/id 1 :person/age] 22)
 
-  (swap! (::app/state-atom APP) update-in [:person/id id :person/age] inc )
+  (swap! (::app/state-atom APP) update-in [:person/id id :person/age] inc)
 
-;; operate simply on Adam
+  ;; operate simply on Adam
   (comp/transact! APP [(make-older {:person/id 1})
                        ;; other mutations
                        ])
 
 
   (comp/component-options Car)
-
 
   )
