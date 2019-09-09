2c66e3c7b9ff2a2574a2b0a2b276a7561a2dc304
Abhinav Sharma (abhi) <abhi18av@gmail.com>
2019-09-10 02:58:52 +0530

    [setup] add dotfiles and configs

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((clojure-mode
+  (cider-jack-in-default . 'clojure-cli)
+  (cider-clojure-cli-global-options . "-A:dev")))
